afc508e56384846f787e57371bf764439d20e79f
Vitaly Juno <vvjuhno@gmail.com>
2024-04-12 08:59:45 +0300

    pathfinding wip

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,10 +1,27 @@
 extends CharacterBody2D
 
 @export var health: float = 4
+@export var speed = 100
+@export var player: Node2D
+@onready var navAgent := $NavigationAgent2D
+
+
 const type = 'enemy'
 
 func _ready():
 	$Label.text = str(health)
+	makePath()
+	
+func _physics_process(delta: float) -> void:
+	var dir = navAgent.get_next_path_position().normalized()
+	#var dir = Vector2(0, -4).normalized()
+	velocity = dir * speed
+	print(navAgent.get_next_path_position())
+	move_and_slide()
+
+func makePath() -> void:
+	navAgent.target_position = player.global_position
+
 
 func damage(amount):
 	health -= amount
@@ -14,3 +31,7 @@ func damage(amount):
 		self.queue_free()
 		
 	
+
+
+func _on_timer_timeout():
+	makePath()
