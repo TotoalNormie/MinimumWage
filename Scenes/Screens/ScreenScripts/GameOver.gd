995ab796dc20712df5cd3837deef676ab533c608
Lxgends <marcitis24@gmail.com>
2024-05-17 09:26:35 +0300

    dsasda
    sdsdasda

--- Scenes/Screens/ScreenScripts/GameOver.gd
+++ Scenes/Screens/ScreenScripts/GameOver.gd
@@ -6,6 +6,7 @@ func _on_game_on_player_death(level):
 	get_tree().paused = true
 	ui.visible = false
 	$AnimationPlayer.play("gameover")
+	%ReachedLevelLabel.text = str("You reached level ", level)
 
 func _on_restart_button_pressed():
 	get_tree().paused = false
@@ -16,3 +17,4 @@ func _on_menu_button_pressed():
 	get_tree().paused = false
 	ui.visible = true
 	get_tree().change_scene_to_file("res://Scenes/Screens/StartScreen.tscn")
+
